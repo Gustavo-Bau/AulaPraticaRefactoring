120ef0db7f4364f5cc74fb87f8c1b3008086980f
Gustavo-Bau <gbau185@gmail.com>
2025-11-23 18:40:14 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,25 +5,38 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
       _title = title;
-      _priceCode = priceCode;
+      setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
+      return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
   }
 
   public String getTitle (){
       return _title;
   }
 
+  // Current implementation still calculates charge here (will be moved to Price in next steps)
   public double getCharge(int daysRented) {
       double result = 0;
       switch (getPriceCode()) {
