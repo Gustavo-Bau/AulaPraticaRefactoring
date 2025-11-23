6639c5b527194a7bdcd4e4edc4cdc1c1ef455fd9
Gustavo-Bau <gbau185@gmail.com>
2025-11-23 18:44:16 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -1,4 +1,10 @@
 public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);
+
+   // moved from Movie.getFrequentRenterPoints(int)
+   public int getFrequentRenterPoints(int daysRented) {
+       if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) return 2;
+       return 1;
+   }
 }
